77a408f3ca37f73ec1f57441a52f456f8337da18
Joshua <joshua.soehn@gmail.com>
2015-04-03 15:32:01 +0200

    new copy & new footer area

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-$( document ).ready(function() {
-
-$('a[href^="http://"]').not('a[href*=gusdecool]').attr('target','_blank');
-$('a[href^="https://"]').not('a[href*=gusdecool]').attr('target','_blank');
-
+$(document).ready(function() {
+  $('a[href^="http://"]').not('a[href*=gusdecool]').attr('target','_blank');
+  $('a[href^="https://"]').not('a[href*=gusdecool]').attr('target','_blank');
 });
