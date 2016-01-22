50558f04bb962c378a89823bc9effeed112c783d
Joshua <joshua.soehn@gmail.com>
2016-01-22 12:20:10 +0100

    converted to middleman

--- js/scripts.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-  $('a[href^="http://"]').not('a[href*=gusdecool]').attr('target','_blank');
-  $('a[href^="https://"]').not('a[href*=gusdecool]').attr('target','_blank');
-});
