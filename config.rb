cab8a6c566dcf4075e9c7bcec5acf07ce9abb319
Joshua <joshua.soehn@gmail.com>
2016-02-10 11:38:56 +0100

    style tweaks, copy

--- config.rb
+++ config.rb
@@ -35,10 +35,9 @@
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
